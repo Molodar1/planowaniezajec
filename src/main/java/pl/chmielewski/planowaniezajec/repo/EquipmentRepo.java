9da9e028c2aad7e519795b352786e9535b91b262
Jakub Chmielewski <j.chmielewski1008@gmail.com>
2020-06-01 14:19:51 +0200

    post project v1.1 01.06.2020

--- src/main/java/pl/chmielewski/planowaniezajec/repo/EquipmentRepo.java
+++
@@ -1,7 +0,0 @@
-package pl.chmielewski.planowaniezajec.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.chmielewski.planowaniezajec.model.Equipment;
-
-public interface EquipmentRepo extends JpaRepository<Equipment,Long> {
-}
